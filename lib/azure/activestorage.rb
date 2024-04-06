85303e7a2c17dd4accfec1431c4fddd6a2655421
Joé Dupuis <joe@dupuis.io>
2024-04-05 19:09:57 -0700

    Initial commit

---
+++ lib/azure/activestorage.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "activestorage/version"
+
+module Azure
+  module Activestorage
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
