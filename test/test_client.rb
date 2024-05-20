71618cd447b223e02655227e584d9f4103c68b75
Joé Dupuis <joe@dupuis.io>
2024-05-20 13:19:37 -0700

    Proper tests WIP

---
+++ test/test_client.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "securerandom"
+
+class TestClient < Minitest::Test
+  attr_reader :client, :key
+
+  def setup
+    @account_name = ENV["AZURE_ACCOUNT_NAME"]
+    @access_key = ENV["AZURE_ACCESS_KEY"]
+    @container = ENV["AZURE_CONTAINER"]
+    @client = AzureBlobStorage::Client.new(
+      account_name: @account_name,
+      access_key: @access_key,
+      container: @container,
+    )
+  end
+
+  def teardown
+    client.delete_blob(key)
+  end
+
+  def test_single_upload
+    @key = 'test_client#test_single_upload'
+    content = "single upload content"
+
+    client.create_block_blob(key, content)
+
+    assert_equal content, client.get_blob(key)
+  end
+end
