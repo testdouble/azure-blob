763bbe87bdd2d21ebe5c5511944aae3b8b8d5075
Joé Dupuis <joe@dupuis.io>
2024-05-27 18:24:17 -0700

    Representation controller test

---
+++ test/rails/controllers/representations/redirect_controller_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails/test_helper"
+require "rails/database/setup"
+
+class ActiveStorage::Representations::RedirectControllerWithOpenRedirectTest < ActionDispatch::IntegrationTest
+  test "showing existing variant stored in azure" do
+    with_raise_on_open_redirects(:azure) do
+      blob = create_file_blob filename: "racecar.jpg", service_name: :azure
+
+      get rails_blob_representation_url(
+        filename: blob.filename,
+        signed_blob_id: blob.signed_id,
+        variation_key: ActiveStorage::Variation.encode(resize_to_limit: [100, 100]))
+
+      assert_redirected_to(/racecar\.jpg/)
+    end
+  end
+end
