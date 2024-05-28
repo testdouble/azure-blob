70856899060939ca0160372bbe5edb8ab729f418
Joé Dupuis <joe@dupuis.io>
2024-05-27 17:57:59 -0700

    Extract direct upload controller tests

---
+++ test/rails/controllers/direct_uploads_controller_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails/test_helper"
+require "rails/database/setup"
+
+
+class ActiveStorage::AzureBlobDirectUploadsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @config = SERVICE_CONFIGURATIONS[:azure]
+    ActiveStorage::Blob.service = ActiveStorage::Service.configure(:azure, SERVICE_CONFIGURATIONS)
+  end
+
+  test "creating new direct upload" do
+    checksum = OpenSSL::Digest::MD5.base64digest("Hello")
+    metadata = {
+      "foo" => "bar",
+      "my_key_1" => "my_value_1",
+      "my_key_2" => "my_value_2",
+      "platform" => "my_platform",
+      "library_ID" => "12345"
+    }
+
+    post rails_direct_uploads_url, params: { blob: {
+      filename: "hello.txt", byte_size: 6, checksum: checksum, content_type: "text/plain", metadata: metadata } }
+
+    response.parsed_body.tap do |details|
+      assert_equal ActiveStorage::Blob.find(details["id"]), ActiveStorage::Blob.find_signed!(details["signed_id"])
+      assert_equal "hello.txt", details["filename"]
+      assert_equal 6, details["byte_size"]
+      assert_equal checksum, details["checksum"]
+      assert_equal metadata, details["metadata"]
+      assert_equal "text/plain", details["content_type"]
+      assert_match %r{#{@config[:storage_account_name]}\.blob\.core\.windows\.net/#{@config[:container]}}, details["direct_upload"]["url"]
+      assert_equal({ "Content-Type" => "text/plain", "Content-MD5" => checksum, "x-ms-blob-content-disposition" => "inline; filename=\"hello.txt\"; filename*=UTF-8''hello.txt", "x-ms-blob-type" => "BlockBlob" }, details["direct_upload"]["headers"])
+    end
+  end
+end
