70ca9cce279c2700d5fe620f6124500c61d7a8f3
Joé Dupuis <joe@dupuis.io>
2024-08-15 16:04:15 -0700

    Script to rsync the working directory to the test vm

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,11 @@
     azure-cli
   ];
 
+  scripts.sync-vm.exec = ''
+    vm_username=$(terraform output --raw "vm_username")
+    vm_ip=$(terraform output --raw "vm_ip")
+    rsync -avx --progress --exclude .devenv . $vm_username@$vm_ip:azure-blob/
+  '';
 
   scripts.generate-env-file.exec = ''
     terraform output -raw devenv_local_nix > devenv.local.nix
