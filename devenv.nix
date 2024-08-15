cfa9f84e74227c82af1895efc6d816781b15e3c5
Joé Dupuis <joe@dupuis.io>
2024-08-15 16:03:52 -0700

    Script to generate the devenv.local.nix file

--- devenv.nix
+++ devenv.nix
@@ -9,6 +9,10 @@
   ];
 
 
+  scripts.generate-env-file.exec = ''
+    terraform output -raw devenv_local_nix > devenv.local.nix
+  '';
+
   languages.ruby.enable = true;
   languages.ruby.version = "3.1.5";
 }
